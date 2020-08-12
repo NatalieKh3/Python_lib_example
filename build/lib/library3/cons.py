34153eb15a29a479d71b112aadce9ce76358db22
Your Name <you@example.com>
2020-08-12 20:06:19 +0600

    fnkk

--- build/lib/library3/cons.py
+++ build/lib/library3/cons.py
@@ -1,6 +1,6 @@
 from kafka import KafkaConsumer
 import json
-def consume_kafka_message("topik11","user5"):
+def consume_kafka_message(topikName,group_id):
     servers=["localhost:9092"]
     topicName="topik11"
     consumer=KafkaConsumer(topicName, group_id="user5", bootstrap_servers=servers, auto_offset_reset="earliest"
