33057e59328acd4eaea0690a73c5b9e9de49eedd
Your Name <you@example.com>
2020-08-12 11:27:09 +0600

    add new elements

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name="library1",
+    version="0.0.1",
+    description="this library is for fun",
+    author="natalie",
+    author_email="hvannatalya@gmail.com",
+    url="https://github.com/NatalieKh3/Python_lib_example",
+    license="MIT",
+    packages=["library1"],
+    install_requires=[]
+)
